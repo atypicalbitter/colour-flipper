d10871484ac80c726659c31af3d80a9df8b99cdd
Matt Dudman <mattdudman95@googlemail.com>
2024-07-14 14:17:28 +0100

    JS logic now added.

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 let body = document.getElementsByTagName("body")[0];
 body.style.backgroundColor = "red"; 
+
+function setColour(name) {
+    body.style.backgroundColor = name; 
+}
+
+function randomColour() {
+const red = Math.round(Math.random() * 255); 
+const green = Math.round(Math.random() * 255); 
+const blue = Math.round(Math.random() * 255); 
+
+const colour = `rgb(${red}, ${green}, ${blue})`
+
+body.style.backgroundColor = colour; 
+}
+
+randomColour(); 
